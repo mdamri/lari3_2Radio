de7756064412b6e1982d9ae69a9e76e145bad046
ahmedteling <derbala.ahmed531992@gmail.com>
2015-12-06 11:34:29 +0100

    lapplication ne fonctionne pas avec l' instruction ancient de background
    alors je l'ai modifie par #ffffff . ahmed

--- app/src/main/java/com/app/ppt/yousoft/radiotn/MainActivity.java
+++ app/src/main/java/com/app/ppt/yousoft/radiotn/MainActivity.java
@@ -1,6 +1,7 @@
 package com.app.ppt.yousoft.radiotn;
 /// premiere version
 ///checked by jassem ben ali
+
 import android.content.Context;
 import android.content.DialogInterface;
 import android.net.ConnectivityManager;
